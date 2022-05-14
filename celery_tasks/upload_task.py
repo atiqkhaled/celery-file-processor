3d1e638b17344285d1fe9fc5c0a5fad48193e63f
Atiqur Rahman <atiqur.rahman1@bd.ey.com>
2022-05-14 15:51:35 +0600

    upload task changes

--- celery_tasks/upload_task.py
+++ celery_tasks/upload_task.py
@@ -7,8 +7,6 @@ import sys
 import os
 sys.path.insert(0, os.path.realpath(os.path.pardir))
 
-
-
 CONNECT_STR = "DefaultEndpointsProtocol=https;AccountName=flexmax;AccountKey=uzaBDMo/m9r+qke6J9asWAqzkLhJ18QdvC3OezDo6o7mHz6MSYw1sFUZhthpPDCB39T/jRAKtDM4+ASttqij1w==;EndpointSuffix=core.windows.net"
 CONTAINER_NAME = "excel-storage"
 blob_service_client = BlobServiceClient.from_connection_string(CONNECT_STR)
